5b076c94658701bffed43ea548d8d1911dd948de
Caio Lima <caiooalexandre94@gmail.com>
2025-06-19 19:51:53 -0300

    feat: Add new activities and layouts for confirmation, product details, restaurant details, and login

--- app/src/main/java/com/example/projeto_boostcamp/presentation/detalhes_restaurantes/DetailsRestaurantsActivity.kt
+++ app/src/main/java/com/example/projeto_boostcamp/presentation/detalhes_restaurantes/DetailsRestaurantsActivity.kt
@@ -1,16 +1,17 @@
-package com.example.projeto_boostcamp
+package com.example.projeto_boostcamp.presentation.detalhes_restaurantes
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projeto_boostcamp.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsRestaurantsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details_restaurants)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
