2eb1fcea0d77656ea0ead5aeb6d7c1537db94868
Sostenes <sostenes1997@gmail.com>
2025-09-07 00:46:43 -0300

    feat: commit dos itens

--- app/src/main/java/com/example/projeto_boostcamp/presentation/detalhes_restaurantes/DetalisRestaurantsRecyclerActivity.kt
+++ app/src/main/java/com/example/projeto_boostcamp/presentation/detalhes_restaurantes/DetalisRestaurantsRecyclerActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projeto_boostcamp.R
+import com.example.projeto_boostcamp.presentation.perfil.Opcao
 
 class DetalisRestaurantsRecyclerActivity : AppCompatActivity() {
 
@@ -19,13 +20,17 @@ class DetalisRestaurantsRecyclerActivity : AppCompatActivity() {
 
         val List =
 
-        rvList = findViewById(R.id.rv_detalhe_produto)
-        rvList.adapter = Itens_restarantsAdapter()
-        //rvList.layoutManager
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            private var listaProduto = mutableListOf(
+            Produto(R.drawable.frango, "Bowl frango tarê", "Bowl com saboroso arroz Yakimeshi, o nosso tradicional arroz soltinho refogado com flocos de ovos, cubinhos de cenoura, presunto, cebolinha picada com óleo de gergelim, coberto com pedaços de frango desossado coberto com molho tarê, gengibre e cebolinha picada.\n" +
+                    "Serve 1 pessoa\n" +
+                    "(350g)","R$37,90"),
+            Produto(R.drawable.peixe, "Macarrão oriental taiwan", "Macarrão com molho à base de shoyu cobertos com porção de pedaços de lombo crispy ou frango crispy e cebolinha picada. Massa tradicional\n" +
+                    "Serve 1 pessoa", "R$37,70"),
+            Produto(R.drawable.br_colis, "Bowl Carne com Brócolis", "Bowl com saboroso arroz Yakimeshi, o nosso tradicional arroz soltinho refogado com flocos de ovos, cubinhos de cenoura, presunto, cebolinha picada com óleo de gergelim, coberto com fatias de carne, champignons e brócolis, com o nosso molho exclusivo à base de shoyu e alho.\n" +
+                    "Serve 1 pessoa\n" +
+                    "(350g)""R$39,90"),
+            Produto(R.drawable.yakisoba, "Yakisoba bowl", "O Yakisoba mais pedido em um novo formato, macarrão oriental, com carne e frango. Acompanhado por legumes frescos (cenoura, brócolis, acelga, repolho) e cogumelos, finalizados com molho à base de shoyu. Alérgicos, nosso molho contém crustáceo, peixe, soja e derivados. Pode conter trigo, ovos e leite.","R$39,90"),
+
         }
     }
 }
